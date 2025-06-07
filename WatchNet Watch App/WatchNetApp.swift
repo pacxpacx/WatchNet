8a04cdfffecc2a9b7c72f97ce4e2c420f4ed36cc
MIA <bo110crestron@gmail.com>
2025-06-07 14:19:54 +0200

    Initial Commit

---
+++ WatchNet Watch App/WatchNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchNetApp.swift
+//  WatchNet Watch App
+//
+//  Created by Pavel Cintins on 07/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct WatchNet_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
